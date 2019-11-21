fefc01ddad5ab4869d0248cb7cf757d151847830
MarcusJones <jones.0bj3@gmail.com>
2019-11-21 13:58:00 +0100

    update add picture

--- jupyterlab_apod_mj/src/index.ts
+++ jupyterlab_apod_mj/src/index.ts
@@ -2,6 +2,24 @@ import {
   JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+  ICommandPalette, MainAreaWidget
+} from '@jupyterlab/apputils';
+
+import {
+  Widget,
+  // PanelLayout
+} from '@phosphor/widgets';
+
+interface APODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+};
+
 
 /**
  * Initialization data for the jupyterlab_apod_mj extension.
@@ -9,8 +27,57 @@ import {
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_apod_mj',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_apod_mj is activated!');
+
+    // Build the MainAreaWidget
+    const content = new Widget();
+    const widget = new MainAreaWidget({content});
+    widget.id = 'apod-jupyterlab-mj';
+    widget.title.label = 'Astronomy pic';
+    widget.title.closable = true;
+
+    // Add image
+    let img = document.createElement('img');
+    content.node.appendChild(img);
+
+    // Random date
+    function randomDate(){
+      const start = new Date(2010, 1, 1);
+      const end = new Date();
+      const randomDate = new Date(start.getTime() + Math.random()*(end.getTime() - start.getTime()));
+      return randomDate.toISOString().slice(0, 10);
+    }
+
+    // Get information about picture
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`);
+    const data = await response.json() as APODResponse;
+
+    if (data.media_type === 'image') {
+      // Populate the image
+      img.src = data.url;
+      img.title = data.title;
+    } else {
+      console.log('Random APOD not a picture');
+    }
+
+    // Add an app command
+    const command: string = 'apod:open';
+    app.commands.addCommand(command, {
+      label: 'Random Astro Pic',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach it to main work area
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    })
+
+    console.log('ICommandPalette:', palette);
+    palette.addItem({command, category: 'Tutorial'});
   }
 };
 
